35cd89ad9f00d4611ab67a030501a99aa4a68cb3
Jelmar <jelmarrapis47@gmail.com>
2025-10-16 21:33:36 +0800

    change pages url

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
-    signIn: "auth/signin",
+    signIn: "/auth/signin",
   },
   session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
   secret: process.env.NEXTAUTH_SECRET,
